3d9c6c455765711d1cb0db88792698633bf987ec
Murilo_Silva <murilo.vfs.472@gmail.com>
2024-05-20 20:44:26 -0300

    Alterações realizadas em sala junto do professor

--- proj-flutter-2024/lib/tela_login.dart
+++ proj-flutter-2024/lib/tela_login.dart
@@ -24,164 +24,153 @@ class _TelaLoginState extends State<TelaLogin> {
       debugShowCheckedModeBanner: false,
       home: Builder(builder: (context) {
         return Scaffold(
-          backgroundColor: Color.fromARGB(255, 2, 252, 11),
+          backgroundColor: Color.fromRGBO(25, 25, 25, 25),
           body: Center(
-            child: SingleChildScrollView(
-              child: Column(
-                children: [
-                  Container(
-                    margin: const EdgeInsets.all(20),
+            child: Column(
+              children: [
+                Image.network(
+                    'https://unicv.edu.br/wp-content/uploads/2020/12/logo-verde-280X100.png',
                     width: 200,
-                    child: const Icon(
-                      Icons.message,
-                      size: 200,
-                      color: Colors.white,
-                    ),
-                  ),
-                  Card(
-                    margin: const EdgeInsets.all(20),
-                    child: Padding(
-                      padding: const EdgeInsets.all(16),
-                      child: Form(
-                        key: _chaveForm,
-                        child: Column(
-                          children: [
+                    height: 200),
+                Card(
+                  margin: const EdgeInsets.all(20),
+                  child: Padding(
+                    padding: const EdgeInsets.all(16),
+                    child: Form(
+                      key: _chaveForm,
+                      child: Column(
+                        children: [
+                          TextFormField(
+                            decoration: const InputDecoration(
+                              labelText: 'Endereço de Email',
+                            ),
+                            keyboardType: TextInputType.emailAddress,
+                            autocorrect: false,
+                            textCapitalization: TextCapitalization.none,
+                            validator: (valor) {
+                              if (valor == null ||
+                                  valor.trim().isEmpty ||
+                                  !valor.contains('@')) {
+                                return 'Por favor, insira um endereço de email válido!';
+                              }
+                              return null;
+                            },
+                            onSaved: (valorDigitado) {
+                              if (valorDigitado != null) {
+                                _email = valorDigitado;
+                              }
+                            },
+                          ),
+                          TextFormField(
+                            decoration:
+                                const InputDecoration(labelText: 'Senha'),
+                            obscureText: true,
+                            validator: (valor) {
+                              if (valor == null || valor.trim().length < 6) {
+                                return 'A senha deve ter pelo menos 6 caracteres.';
+                              }
+                              return null;
+                            },
+                            onSaved: (valor) {
+                              if (valor != null) {
+                                _senha = valor;
+                              }
+                            },
+                          ),
+                          if (!_modoLogin)
                             TextFormField(
                               decoration: const InputDecoration(
-                                labelText: 'Endereço de Email',
-                              ),
-                              keyboardType: TextInputType.emailAddress,
-                              autocorrect: false,
-                              textCapitalization: TextCapitalization.none,
+                                  labelText: 'Nome de Usuário'),
                               validator: (valor) {
-                                if (valor == null ||
-                                    valor.trim().isEmpty ||
-                                    !valor.contains('@')) {
-                                  return 'Por favor, insira um endereço de email válido!';
-                                }
-                                return null;
-                              },
-                              onSaved: (valorDigitado) {
-                                if (valorDigitado != null) {
-                                  _email = valorDigitado;
-                                }
-                              },
-                            ),
-                            TextFormField(
-                              decoration:
-                                  const InputDecoration(labelText: 'Senha'),
-                              obscureText: true,
-                              validator: (valor) {
-                                if (valor == null || valor.trim().length < 6) {
-                                  return 'A senha deve ter pelo menos 6 caracteres.';
+                                if (valor == null || valor.trim().length < 4) {
+                                  return 'O nome de usuário deve ter pelo menos 4 caracteres.';
                                 }
                                 return null;
                               },
                               onSaved: (valor) {
                                 if (valor != null) {
-                                  _senha = valor;
+                                  _nomeUsuario = valor;
                                 }
                               },
                             ),
-                            if (!_modoLogin)
-                              TextFormField(
-                                decoration: const InputDecoration(
-                                    labelText: 'Nome de Usuário'),
-                                validator: (valor) {
-                                  if (valor == null ||
-                                      valor.trim().length < 4) {
-                                    return 'O nome de usuário deve ter pelo menos 4 caracteres.';
+                          const SizedBox(height: 12),
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: [
+                              ElevatedButton(
+                                onPressed: () async {
+                                  if (!_chaveForm.currentState!.validate()) {
+                                    return;
                                   }
-                                  return null;
-                                },
-                                onSaved: (valor) {
-                                  if (valor != null) {
-                                    _nomeUsuario = valor;
+                                  _chaveForm.currentState!.save();
+
+                                  try {
+                                    if (_modoLogin) {
+                                      //logar usuário
+                                      print(
+                                          'Usuario com email $_email e senha $_senha Logado!');
+                                      await _firebaseAuth
+                                          .signInWithEmailAndPassword(
+                                              email: _email, password: _senha);
+                                    } else {
+                                      //criar usuário
+                                      print(
+                                          'Usuario $_nomeUsuario criado com email $_email e senha $_senha');
+                                      final credenciaisUsuario =
+                                          await _firebaseAuth
+                                              .createUserWithEmailAndPassword(
+                                                  email: _email,
+                                                  password: _senha);
+
+                                      await FirebaseFirestore.instance
+                                          .collection('usuarios')
+                                          .doc(credenciaisUsuario.user!.uid)
+                                          .set({
+                                        'email': _email,
+                                        'isAdmin': false,
+                                        'usuario': _nomeUsuario,
+                                      });
+                                    }
+                                  } on FirebaseAuthException catch (error) {
+                                    String mensagem =
+                                        'Falha no Registro de novo Usuário';
+                                    if (error.code == 'email-already-in-use') {
+                                      mensagem = 'Email já utilizado';
+                                    }
+                                    ScaffoldMessenger.of(context)
+                                        .clearSnackBars();
+                                    ScaffoldMessenger.of(context).showSnackBar(
+                                      SnackBar(
+                                        content: _modoLogin
+                                            ? const Text('Falha no Login')
+                                            : Text(mensagem),
+                                      ),
+                                    );
                                   }
                                 },
+                                child: _modoLogin
+                                    ? const Text('Entrar')
+                                    : const Text('Salvar'),
                               ),
-                            const SizedBox(height: 12),
-                            Row(
-                              mainAxisAlignment: MainAxisAlignment.center,
-                              children: [
-                                ElevatedButton(
-                                  onPressed: () async {
-                                    if (!_chaveForm.currentState!.validate()) {
-                                      return;
-                                    }
-                                    _chaveForm.currentState!.save();
-
-                                    try {
-                                      if (_modoLogin) {
-                                        //logar usuário
-                                        print(
-                                            'Usuario com email $_email e senha $_senha Logado!');
-                                        await _firebaseAuth
-                                            .signInWithEmailAndPassword(
-                                                email: _email,
-                                                password: _senha);
-                                      } else {
-                                        //criar usuário
-                                        print(
-                                            'Usuario $_nomeUsuario criado com email $_email e senha $_senha');
-                                        final credenciaisUsuario =
-                                            await _firebaseAuth
-                                                .createUserWithEmailAndPassword(
-                                                    email: _email,
-                                                    password: _senha);
-
-                                        await FirebaseFirestore.instance
-                                            .collection('usuarios')
-                                            .doc(credenciaisUsuario.user!.uid)
-                                            .set({
-                                          'email': _email,
-                                          'isAdmin': false,
-                                          'usuario': _nomeUsuario,
-                                        });
-                                      }
-                                    } on FirebaseAuthException catch (error) {
-                                      String mensagem =
-                                          'Falha no Registro de novo Usuário';
-                                      if (error.code ==
-                                          'email-already-in-use') {
-                                        mensagem = 'Email já utilizado';
-                                      }
-                                      ScaffoldMessenger.of(context)
-                                          .clearSnackBars();
-                                      ScaffoldMessenger.of(context)
-                                          .showSnackBar(
-                                        SnackBar(
-                                          content: _modoLogin
-                                              ? const Text('Falha no Login')
-                                              : Text(mensagem),
-                                        ),
-                                      );
-                                    }
-                                  },
-                                  child: _modoLogin
-                                      ? const Text('Entrar')
-                                      : const Text('Salvar'),
-                                ),
-                                const SizedBox(width: 12),
-                                ElevatedButton(
-                                  onPressed: () {
-                                    setState(() {
-                                      _modoLogin = !_modoLogin;
-                                    });
-                                  },
-                                  child: _modoLogin
-                                      ? const Text('Criar uma conta')
-                                      : const Text('Já tenho uma conta'),
-                                ),
-                              ],
-                            ),
-                          ],
-                        ),
+                              const SizedBox(width: 12),
+                              ElevatedButton(
+                                onPressed: () {
+                                  setState(() {
+                                    _modoLogin = !_modoLogin;
+                                  });
+                                },
+                                child: _modoLogin
+                                    ? const Text('Criar uma conta')
+                                    : const Text('Já tenho uma conta'),
+                              ),
+                            ],
+                          ),
+                        ],
                       ),
                     ),
                   ),
-                ],
-              ),
+                ),
+              ],
             ),
           ),
         );
